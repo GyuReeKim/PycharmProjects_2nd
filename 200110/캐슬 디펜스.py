35254033eadbfd36648c44fef184a9ae9d483def
GyuReeKim <starkim96@naver.com>
2020-01-10 16:01:19 +0900

    200110

---
+++ 200110/캐슬 디펜스.py
@@ -0,0 +1,19 @@
+# 백준 - 17135번
+
+
+def f(n, m, k):
+    if n == k:
+        print(archer)
+    else:
+        for i in range(n, m):
+            archer[i] = 1
+            f(n+1, m, k)
+            archer[i] = 0
+
+
+N, M, D = map(int, input().split())
+board = [list(input().split()) for _ in range(N)]
+print(board)
+
+archer = [0]*M
+f(0, 0, M, 3) # 시작 인덱스,
